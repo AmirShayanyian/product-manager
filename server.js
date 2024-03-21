c467e9ae61dd7cb67c4bc638d6710febbba47f00
amirshayanyian <wahtamir12@gmail.com>
2024-03-21 11:27:16 +0330

    prepared server.js for handling requests

--- server.js
+++ server.js
@@ -1,6 +1,11 @@
 const http = require("http");
+const products = require("./data/product");
 const PORT = 3000;
-const server = http.createServer((req, res) => {});
+const server = http.createServer((req, res) => {
+  console.log(req.url);
+  res.writeHead(200, { "content-type": "application/json" });
+  res.end(JSON.stringify(products));
+});
 
 server.listen(PORT);
-console.log(`Server is running on port ${PORT}`);
+console.log(`Server is running on port ${PORT} http://localhost:${PORT}`);
